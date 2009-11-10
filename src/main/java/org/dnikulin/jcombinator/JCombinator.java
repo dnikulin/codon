689e53b37ebec4b245acc4219373e1f2ec149a23
Dmitri Nikulin <dnikulin@gmail.com>
2009-11-10 15:58:05 +1100

    Initiate JCombinator project

---
+++ src/main/java/org/dnikulin/jcombinator/JCombinator.java
@@ -0,0 +1,5 @@
+package org.dnikulin.jcombinator;
+
+public class JCombinator {
+
+}
---
+++ src/test/java/org/dnikulin/jcombinator/JCombinatorTest.java
@@ -0,0 +1,9 @@
+package org.dnikulin.jcombinator;
+
+import org.junit.Test;
+
+public class JCombinatorTest {
+    @Test
+    public void testNothing() {
+    }
+}
