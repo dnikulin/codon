cda296a44f58effafc5d444db6e6c10ad47bf1fa
Dmitri Nikulin <dnikulin@gmail.com>
2009-12-02 10:59:28 +1100

    Create StreamObjectFormat abstract class
    An ObjectFormat implemented using DataInputStream
    and DataOutputStream.

---
+++ src/main/java/org/dnikulin/codon/format/StreamObjectFormat.java
@@ -0,0 +1,81 @@
+// Copyright (c) 2009 Dmitri Nikulin
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package org.dnikulin.codon.format;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/** An ObjectFormat implemented using DataInputStream and DataOutputStream. */
+public abstract class StreamObjectFormat<T> implements ObjectFormat {
+    @Override
+    @SuppressWarnings("unchecked")
+    public byte[] encode(Object object) {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(bos);
+
+        try {
+            write(out, (T) object);
+            out.flush();
+            return bos.toByteArray();
+        } catch (IOException ex) {
+            return null;
+        }
+    }
+
+    @Override
+    public Object decode(byte[] bytes) {
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        DataInputStream in = new DataInputStream(bis);
+
+        try {
+            return read(in);
+        } catch (IOException ex) {
+            return null;
+        }
+    }
+
+    /**
+     * Read an object from a data input stream.
+     * 
+     * @param in
+     *            Input stream
+     * @return Object
+     */
+    public abstract T read(DataInputStream in) throws IOException;
+
+    /**
+     * Write an object to a data output stream.
+     * 
+     * @param out
+     *            Output stream
+     * @param object
+     *            Object
+     */
+    public abstract void write(DataOutputStream out, T object)
+            throws IOException;
+}
